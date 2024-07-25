340f661c59f99b8cc3f4d2bd0f0f8cf15c30e29a
jack-pardini <jack.pardini@gmail.com>
2024-07-25 12:26:23 -0700

    added api data, removed hardcoded movie data

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,7 @@
+// Import the PropTypes library 
+import PropTypes from "prop-types";
+
+// The MovieCard function component
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div
@@ -8,4 +12,16 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       {movie.Title}
     </div>
   );
+};
+
+// Here is where we define all the props contraints for the MovieCard
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    ImageURL: PropTypes.string.isRequired,
+    Director: PropTypes.string.isRequired,
+    Genre: PropTypes.string.isRequired,
+    Year: PropTypes.string,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
 };
