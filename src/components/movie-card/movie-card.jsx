07e02f4889614d1a53c2a1f83b297010b7178c94
jack-pardini <157051494+jack-pardini@users.noreply.github.com>
2024-07-25 14:15:52 -0700

    Revert "Br api data"

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,7 +1,3 @@
-// Import the PropTypes library 
-import PropTypes from "prop-types";
-
-// The MovieCard function component
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div
@@ -12,26 +8,4 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       {movie.Title}
     </div>
   );
-};
-
-// Here is where we define all the props contraints for the MovieCard
-MovieCard.propTypes = {
-  movie: PropTypes.shape({
-    Title: PropTypes.string.isRequired,
-    Description: PropTypes.string.isRequired,
-    ImageURL: PropTypes.string.isRequired,
-    Year: PropTypes.string.isRequired,
-    Featured: PropTypes.boolean,
-    Genre: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Description: PropTypes.string.isRequired
-    }),
-    Director: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Bio: PropTypes.string.isRequired,
-      Birth: PropTypes.string.isRequired,
-      Death: PropTypes.string
-    }),
-  }).isRequired,
-  onMovieClick: PropTypes.func.isRequired,
 };
